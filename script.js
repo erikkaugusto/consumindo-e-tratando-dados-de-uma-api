c4b2e5b3e445f7b0849d2c70ddc46c7e53d24687
erikkaugusto <erik.ahlert@universo.univates.br>
2024-06-05 16:26:02 -0300

    utilizando async/await

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const containerVideos = document.querySelector(".videos__container");
 
-const api = fetch("http://localhost:3000/videos") // vai buscar nesse endereço
-    .then(res => res.json())
-    .then((videos) =>
+async function buscarEMostrarVideos() { // código assíncrono
+    try {
+        const busca = await fetch("http://localhost:3000/videos") // vai buscar nesse endereço
+        const videos = await busca.json();
+
         videos.forEach((video) => {
+            // Prevenção personalizada de erros
+            // if (video.categoria == ""){ 
+            //     throw new Error('Vídeo não tem categoria');
+            // }
             containerVideos.innerHTML += `
             <li class="videos__item">
                 <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
@@ -15,7 +21,9 @@ const api = fetch("http://localhost:3000/videos") // vai buscar nesse endereço
             </li>
             `
         })
-    )
-    .catch((error) => { // caso ocorra um erro
-        containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error} </p>`;
-    })
+    } catch (error) {
+        containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}</p>`;
+    }
+}
+
+buscarEMostrarVideos();
